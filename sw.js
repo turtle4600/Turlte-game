89f8baacda6bb8ffce9bd36dc48168d5ad38c193
turtle4600 <decast.gregory@gmail.com>
2025-08-14 12:45:39 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE = 'regulateur-v0.9';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png'
+];
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+});
+self.addEventListener('activate', e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+});
+self.addEventListener('fetch', e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=> r || fetch(e.request).then(resp=>{
+      return resp;
+    }).catch(()=>caches.match('./index.html')))
+  );
+});
